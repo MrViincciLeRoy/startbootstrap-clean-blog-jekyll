aba927b561b3767ea3d5db6f697eeab3f684ee9e
Mr Viincci Le Roy <60152334+MrViincciLeRoy@users.noreply.github.com>
2025-10-20 12:22:28 +0200

    Update bp.py

--- FlaskApp/core/pages/bp.py
+++ FlaskApp/core/pages/bp.py
@@ -2,7 +2,7 @@
 Pages blueprint
 """
 from flask import Blueprint
-from flask_app.core.pages.routes import setup_pages_routes
+from FlaskApp.core.pages.routes import setup_pages_routes
 
 pages_bp = Blueprint('pages', __name__, url_prefix='')
 setup_pages_routes(pages_bp)
