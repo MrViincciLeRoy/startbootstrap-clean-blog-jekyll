7cea9834a2bac3c37fe56fd6a76ac6c752ec5412
Mr Viincci Le Roy <60152334+MrViincciLeRoy@users.noreply.github.com>
2025-10-20 12:12:51 +0200

    Add files via upload

--- FlaskApp/core/dashboard/bp.py
+++ FlaskApp/core/dashboard/bp.py
@@ -2,7 +2,7 @@
 Dashboard blueprint
 """
 from flask import Blueprint
-from flask_app.core.dashboard.routes import setup_dashboard_routes
+from FlaskApp.core.dashboard.routes import setup_dashboard_routes
 
 dashboard_bp = Blueprint('dashboard', __name__, url_prefix='')
 setup_dashboard_routes(dashboard_bp)
--- FlaskApp/core/dashboard/routes.py
+++ FlaskApp/core/dashboard/routes.py
@@ -3,7 +3,7 @@ Dashboard routes
 """
 from flask import render_template
 from flask_login import login_required
-from flask_app.services.github_manager import get_github_manager
+from FlaskApp.services.github_manager import get_github_manager
 
 def setup_dashboard_routes(bp):
     """Setup dashboard routes"""
